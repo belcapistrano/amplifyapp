f90b05a59b3a5d953f13d88cb8b5b77be03049e2
belcapistrano <bebelcapistrano@aol.com>
2021-12-26 20:50:17 -0500

    added cayla

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Where is Penny?
+          Hey Cayla. Where is Penny?
         </p>
         
       </header>
